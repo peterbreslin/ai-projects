1460c045cd1dee76215692e5b815d3f50b1c1a5d
Peter <pfbreslin@gmail.com>
2025-01-22 16:30:17 -0800

    Added the ability to play an agent vs human game

--- RL/tictactoe.py
+++ RL/tictactoe.py
@@ -5,6 +5,14 @@ from pathlib import Path
 # Get the directory of the current file
 directory = Path(__file__).parent.resolve()
 
+"""
+Building an agent to play Tic-Tac-Toe using Q-Learning.
+
+This basic tabular reinforcement learning approach trains the agent with the Q-Learning algorithm, which uses the Bellman Equation to determine and update the Q-values from interacting with a Tic-Tac-Toe environment that is built from scratch. 
+
+The QTable is saved and the agent's performance is then evaluated post-training by playing games against the computer (the computer chooses random moves every time). Finally, the agent plays us, the human, in a command-line game of Tic-Tac-Toe.
+"""
+
 
 class TTTQLearning:
     def __init__(self, N=int(1e7), alpha=0.2, gamma=0.9, epsilon=1.0, epsilon_min=0.01, epsilon_decay=0.999):
@@ -17,6 +25,9 @@ class TTTQLearning:
         self.epsilon_decay = epsilon_decay
 
 
+    # ------------ ------------ ------------ ------------ ------------ --
+    # Helper functions for building and interacting with the environment
+    # ------------ ------------ ------------ ------------ ------------ --
     def possible_actions(self, state):
         """
         Returns a list of available actions (the empty spots on the Tic-Tac-Toe board).
@@ -81,6 +92,9 @@ class TTTQLearning:
                 return (new_state, 0)  # No consequence for Player 1
             
 
+    # ------------ ------------ ----------
+    # Training the agent using Q-Learning
+    # ------------ ------------ ----------
     def train(self):
         """
         Trains the agent using Q-learning and saves the QTable as a pickle dict.
@@ -144,16 +158,17 @@ class TTTQLearning:
         # Save Qtable as a pickle file
         with open('TTT_Qtable.p', 'wb') as file:
             pickle.dump(self.Qtable, file)
-    
 
+        
+    # ------------ ------------ ------------ ------------ ---
+    # Helper functions for playing an Agent vs Computer game
+    # ------------ ------------ ------------ ------------ ---
     def agent_vs_computer(self, use_qtable=True):
         """
         The agent will be Player 1; Player 2 is the opponent
         """
         state = [0] * 9
         game_log = []
-        with open(f'{directory}/TTT_Qtable.p', 'rb') as file:
-            Qtable = pickle.load(file)
 
         # Determine who starts
         current_player = 1 if random.random() < 0.5 else 2
@@ -168,8 +183,8 @@ class TTTQLearning:
 
             # Agent's turn
             if current_player == 1 and use_qtable:
-                if state_str in Qtable:
-                    action = max(actions, key=lambda x: Qtable[state_str][x])
+                if state_str in self.Qtable:
+                    action = max(actions, key=lambda x: self.Qtable[state_str][x])
                 else:
                     action = random.choice(actions)  # Random if state not in Qtable
 
@@ -186,10 +201,13 @@ class TTTQLearning:
 
         
     def evaluate(self, n_games = 100):
+
+        with open(f'{directory}/TTT_Qtable.p', 'rb') as file:
+            self.Qtable = pickle.load(file)
+
         wins = 0
         draws = 0
         losses = 0
-
         for _ in range(n_games):
             result, game_log = self.agent_vs_computer(use_qtable=True)
 
@@ -199,7 +217,7 @@ class TTTQLearning:
 
             # Draw
             elif result == 0:
-                draws += 1
+                draws += 11
 
             # The opponent (Player 2) wins
             else:
@@ -210,6 +228,94 @@ class TTTQLearning:
         print(f"The agent lost {losses}")
         print(f"Draws: {draws}")
         print(f"Winning percentage: {(wins / n_games) * 100}%")
+
+
+    # ------------ ------------ ------------ ------------ 
+    # Helper functions for playing an Agent vs Human game
+    # ------------ ------------ ------------ ------------ 
+    def transform_state(self, state):
+        """
+        Takes in current state of the board and swaps the roles of Player 1 (X) and Player 2 (O)
+        """
+        return [2 if x == 1 else 1 if x == 2 else 0 for x in state]
+
+
+    def print_board(self, state):
+        """
+        Prints the current state of the board to the console in a user-friendly format.
+        chars dictionary maps the board values (0, 1, 2) to the characters ' ', 'X', 'O'.
+        Top Row: state[0], state[1], state[2]
+        Mid Row: state[3], state[4], state[5]
+        Bot Row: state[6], state[7], state[8]
+        """
+        chars = {0: ' ', 1: 'X', 2: 'O'}
+        for i in range(3):
+            print(f"{chars[state[3 * i]]} | {chars[state[3 * i + 1]]} | {chars[state[3 * i + 2]]}")
+
+            # Separator rows after each row (except last one)
+            if i < 2:
+                print("---------")
+
+
+    def agent_move(self, state):
+        """
+        Determines agent's move based on the current board state. Move chosen using a Q-table if available or randomly if the state is unknown to the agent.
+        """
+        transformed_state = self.transform_state(state)
+        state_str = str(transformed_state)
+
+        if state_str not in self.Qtable:
+            return random.choice(self.possible_actions(state))
+
+        return max(self.possible_actions(state), key=lambda x: self.Qtable[state_str][x])
+
+
+    def agent_vs_human(self):
+        """
+        Player 1 is the human, Player 2 is the agent
+        """
+        state = [0] * 9
+        current_player = 1 if random.random() < 0.5 else 2
+
+        print("Board positions are indexed as follows:\n")
+        print("0 | 1 | 2")
+        print("---------")
+        print("3 | 4 | 5")
+        print("---------")
+        print("6 | 7 | 8")
+    
+        print("\nWhen prompted to make a move, enter the number corresponding to the position you want to play. For example, entering '0' will place your move in the top-left corner.\n")
+
+        while True:
+            self.print_board(state)
+
+            if current_player == 1:
+                actions = self.possible_actions(state)
+                move = int(input(f"\nEnter your move (0-8): "))
+
+                while move not in actions:
+                    print("\nInvalid move, try again.")
+                    move = int(input(f"Enter your move (0-8): "))
+                state = self.update_state(state, move, 1)
+
+            else:
+                print("\nAgent's turn:")
+                move = self.agent_move(state)
+                state = self.update_state(state, move, 2)
+
+            if self.check_winner(state, current_player):
+                self.print_board(state)
+                if current_player == 1:
+                    print("\nYou win!")
+                else:
+                    print("\nAgent wins!")
+                break
+            elif 0 not in state:
+                self.print_board(state)
+                print("\nIt's a draw!")
+                break
+
+            current_player = 1 if current_player == 2 else 2
 
 
 if __name__ == '__main__':
@@ -229,4 +335,5 @@ if __name__ == '__main__':
     print("Evaluating the agent's performance")
     agent.evaluate()
 
-    # print("Game Time!")
+    print("Game Time!")
+    agent.agent_vs_human()
